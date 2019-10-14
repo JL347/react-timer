a2becc4f396e4a5dd8bd74203598922c2f6ae2cf
Ubuntu <ubuntu@ip-172-31-23-199.us-west-2.compute.internal>
2019-10-14 18:33:51 +0000

    input css

--- react-timer/src/Timer.js
+++ react-timer/src/Timer.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Timer = () => {
+    const [seconds, setSeconds] = useState(0)
+    const [isActive, setIsActive] = useState(false)
     return (
         <div className = "app">
             <div className = "time">
-              seconds
+              {seconds}s
             </div>
             <div className = "row">
                 <button className = "button-primary">
